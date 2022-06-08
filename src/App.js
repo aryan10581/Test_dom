393845aab3991ec7ce776db3265867626c965679
aryan10581 <aryanrajput1058.com>
2022-06-08 12:26:28 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -11,31 +11,19 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import Nav from './Nav';
+import Home from './Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+<Routes>
+
+<Route path='/' element={<Nav/>}/>
+    <Route path='/home' element={<Home/>}/>
+        
+       
+</Routes>  
   );
 }
 
